3de7b4490a9971f82258d910e172777f67751005
DiegoFerneyUsedaDiaz <dfusedad@gmail.com>
2025-07-29 23:54:18 -0500

    configuracion modal y boton para actualizar correctamente los datos en la pagina usuario

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import CentroComercial, Tienda
+from django.contrib.auth.models import User
 
 # Create your views here.
 def lista_centros_comerciales(request):
@@ -16,4 +18,20 @@ def vista_centros_comercial(request, name_cc):
     return render(request, 'vista_centros_comercial.html', {'centro_comercial': centro_comercial, 'tiendas': tiendas})
 
 def profile(request):
+    if request.method == 'POST':
+        data= request.POST
+        new_name1 = data.get('first_name')
+        new_name2 = data.get('last_name')
+        new_email = data.get('email')
+        
+        user_id = request.user.id
+        user = User.objects.get(id=user_id)
+        user.first_name = new_name1
+        user.last_name = new_name2
+        user.email = new_email 
+        user.save()
+        return JsonResponse({'status': 'success'})
+        
     return render(request, 'usuario.html')
+        
+        
